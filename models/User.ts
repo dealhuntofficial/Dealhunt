cc413212189127e7c173de48589d16ad8c88373b
dealhuntofficial <dealhuntofficial@gmail.com>
2025-11-25 01:56:48 +0530

    Add files via upload

--- models/User.ts
+++ models/User.ts
@@ -1,1 +1,12 @@
+import mongoose from "mongoose";
 
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String },
+  image: { type: String },
+  provider: { type: String, default: "credentials" },
+});
+
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
+export default User;
