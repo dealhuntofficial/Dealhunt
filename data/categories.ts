648ad7a065e46850afcaeab15d40151c1fb10128
dealhuntofficial <dealhuntofficial@gmail.com>
2025-11-25 14:56:13 +0530

    Add files via upload

---
+++ data/categories.ts
@@ -0,0 +1,10 @@
+export const categories = [
+  { id: 1, name: "Men’s Watches", slug: "mens-watches", image: "/images/categories/mens-watches.png" },
+  { id: 2, name: "Women’s Watches", slug: "womens-watches", image: "/images/categories/womens-watches.png" },
+  { id: 3, name: "Jewelry", slug: "jewelry", image: "/images/categories/jewelry.png" },
+  { id: 4, name: "Shoes", slug: "shoes", image: "/images/categories/shoes.png" },
+  { id: 5, name: "Bags", slug: "bags", image: "/images/categories/bags.png" },
+  { id: 6, name: "Electronics", slug: "electronics", image: "/images/categories/electronics.png" },
+  { id: 7, name: "Accessories", slug: "accessories", image: "/images/categories/accessories.png" },
+  { id: 8, name: "Others", slug: "others", image: "/images/categories/others.png" },
+];
