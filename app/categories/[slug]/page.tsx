f4812b14c626194d1d39a3ee73d53c05825feb09
dealhuntofficial <dealhuntofficial@gmail.com>
2025-12-15 19:42:52 +0530

    Update page.tsx

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { categories } from "@/data/categories";
 import { subCategories } from "@/data/subcategories";
 import DealCard from "@/components/DealCard";
