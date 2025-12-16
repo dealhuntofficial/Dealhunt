b63de0cd9d27549878911041fed66ef9a5485c26
dealhuntofficial <dealhuntofficial@gmail.com>
2025-12-16 16:40:06 +0530

    Update FiltersBar.tsx

--- components/FiltersBar.tsx
+++ components/FiltersBar.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { subCategories } from "@/config/subCategories";
+import { subCategories } from "@/data/subCategories";
 
 interface Merchant {
   id: string;
